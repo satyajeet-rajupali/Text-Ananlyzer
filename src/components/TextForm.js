0440c8fbff14bc35804911d316ccb17cde337182
satyajeet <satyajeetrajupali@gmail.com>
2023-09-26 21:05:54 +0530

    Added grammar text corrector

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Sapling } from "@saplingai/sapling-js/observer";
 
 const TextForm = (props) => {
     const [text, setText] = useState('');
@@ -24,7 +25,6 @@ const TextForm = (props) => {
 
     const handleOnClickCopy = () => {
         let text = document.getElementById('myBox');
-        text.select();
         navigator.clipboard.writeText(text.value);
         props.showAlert("Text copied to clipboard.", "success");
     }
@@ -47,13 +47,27 @@ const TextForm = (props) => {
     const totalTime = () => {
         return 0.008 * totalWords();
     }
+    // eslint-disable-next-line
+    useEffect(() => {
+        Sapling.init({
+            key: 'QCSG806T0DYMWK47AN0FJ69G0KFCJNVN',
+            endpointHostname: 'https://api.sapling.ai',
+            editPathname: '/api/v1/edits',
+            statusBadge: true,
+            mode: 'dev',
+        });
+
+        const editor = document.getElementById('myBox');
+        Sapling.observe(editor);
+
+    });
 
     return (
         <div className="container" style={{ color: props.mode === 'dark' ? 'white' : '#000000' }}>
             <div className="container" >
                 <h3>{props.heading}</h3>
                 <div className="mb-3">
-                    <textarea className="form-control" placeholder={"Enter your text here...."} value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+                    <textarea className="form-control" sapling-ignore="true" placeholder={"Enter your text here...."} value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
                 </div>
                 <button disabled={text.length === 0} className="btn btn-primary mx-2 my-2" onClick={handleOnClickUppercase} >Convert to Uppercase</button>
                 <button disabled={text.length === 0} className="btn btn-primary mx-1 my-2" onClick={handleOnClickLowercase} >Convert to LowerCase</button>
